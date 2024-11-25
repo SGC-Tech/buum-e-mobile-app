9c4500fec27ee1a6f01f79dcade4d14135387d27
gunayakay <gunayakay@outlook.com>
2024-11-25 17:00:38 +0300

    add the babel plugin and js file, setup the imports in the project

---
+++ babel.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  presets: ["module:@react-native/babel-preset"],
+  env: {},
+  plugins: [
+    [
+      require.resolve("babel-plugin-module-resolver"),
+      {
+        root: ["./"],
+        alias: {
+          "#": "./src",
+          "#app": "./src",
+          "#assets": "./src/assets",
+          "#components": "./src/components",
+          "#hooks": "./src/hooks",
+          "#lang": "./src/lang",
+          "#lib": "./src/lib",
+          "#navs": "./src/navs",
+          "#screens": "./src/screens",
+          "#services": "./src/services",
+          "#static": "./src/static",
+          "#themes": "./src/themes",
+          "#utils": "./src/utils",
+          "#authentication": "./src/features/authentication",
+          "#authorization": "./src/features/authorization",
+          "#announcements": "./src/features/announcements",
+          "#home": "./src/features/home",
+          "#errors": "./src/features/errors",
+          "#profile": "./src/features/profile",
+          "#products": "./src/features/products",
+          "#orders": "./src/features/orders",
+          "#virtual-pos": "./src/features/virtual-pos",
+          "#notifications": "./src/features/notifications",
+        },
+      },
+    ],
+    "react-native-reanimated/plugin",
+  ],
+};
